c4b96226311f1c1a24e40702c8ed229263bfbd38
Michael Rittmeister <michael@rittmeister.in>
2022-12-16 09:18:50 +0100

    Finally, do the thing I was supposed to do ages ago

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,13 @@
+import lejos.utility.Delay;
+
+import java.util.concurrent.TimeUnit;
+
 public class Test {
     public static void main(String[] args) {
         TwoWheelRobot robo = new TwoWheelRobot();
-        robo.drive();
-        Helfer.delayProgramm(5);
+        robo.drive(5);
+        Delay.msDelay(TimeUnit.SECONDS.toMillis(2));
+        robo.setSpeed(-1);
+        robo.drive(5);
     }
 }
--- src/TwoWheelRobot.java
+++ src/TwoWheelRobot.java
@@ -1,11 +1,12 @@
-import java.io.File;
-
 import lejos.hardware.port.Port;
+import lejos.utility.Delay;
+
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class TwoWheelRobot {
 	private final EV3Brick brick = new EV3Brick();
-	private final double speed = 1.0;
+	private double speed = 1.0;
 	private final Motor motA = new Motor();
 	private final Motor motB = new Motor();
 	private final Port portA = brick.getPort("A");
@@ -16,9 +17,28 @@ public class TwoWheelRobot {
 		motB.connect(portB);
 	}
 
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
 	void drive() {
 		motA.start(speed);
 		motB.start(speed);
+	}
+
+	void drive(int seconds) {
+		drive((long) seconds);
+	}
+
+	void drive(long seconds) {
+		drive();
+		Delay.msDelay(TimeUnit.SECONDS.toMillis(seconds));
+		stop();
+	}
+
+	void stop() {
+		motA.stop();
+		motB.stop();
 	}
 
 	void turn() {
