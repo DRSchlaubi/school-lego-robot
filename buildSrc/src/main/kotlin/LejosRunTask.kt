8c158b60f6b2849fd09d3b5edf9452492c89fd50
Michael Rittmeister <michael@rittmeister.in>
2022-12-20 09:41:35 +0100

    Implement call to ev3scpupload.bat

--- buildSrc/src/main/kotlin/LejosRunTask.kt
+++ buildSrc/src/main/kotlin/LejosRunTask.kt
@@ -1,10 +1,9 @@
 package dev.schlaubi.lejos.gradle
 
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
+import lejos
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.TaskAction
 import org.gradle.jvm.tasks.Jar
+import java.io.File
 
 abstract class LejosRunTask : Jar() {
     init {
@@ -14,7 +13,19 @@ abstract class LejosRunTask : Jar() {
         manifest.attributes(mapOf("Class-Path" to "/home/root/lejos/lib/ev3classes.jar /home/root/lejos/lib/opencv-2411.jar /home/root/lejos/lib/dbusjava.jar /home/root/lejos/libjna/usr/share/java/jna.jar"))
 
         from({ project.configurations.getByName("runtimeClasspath").map { if (it.isDirectory()) it else project.zipTree(it) } })
+
+        doLast { startBotTask() }
     }
 
     fun mainClass(name: String) = manifest.attributes(mapOf("Main-Class" to name))
+
+    private fun startBotTask() {
+        project.exec {
+            standardOutput = System.out
+            val base = System.getenv("LEJOS_HOME") ?: error("Please define LEJOS_HOME")
+            commandLine("$base/bin/ev3scpupload.bat")
+            val file = archiveFile.get().asFile
+            args("-n", project.lejos.botIp.get(), "-r", file.absolutePath, "/home/lejos/programs/${file.name}")
+        }
+    }
 }
